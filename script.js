2c89e353c59bc648e3915b314df7fef379e834d3
Fareeda Farsana <fareedafarzaa@gmail.com>
2025-12-26 18:49:04 +0530

    initial commit - navbar, home, booking page

---
+++ script.js
@@ -0,0 +1,26 @@
+// TOGGLE MENU
+function toggleMenu() {
+  const links = document.querySelector('.nav-links');
+  const btn = document.querySelector('.nav-btn');
+  links.classList.toggle('active');
+  btn.classList.toggle('active');
+}
+
+// POPUP
+function showPopup(e){
+  e.preventDefault();
+  document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup(){
+  document.getElementById("popup").style.display = "none";
+}
+
+// close popup when clicking outside card
+document.addEventListener("DOMContentLoaded", function(){
+document.getElementById("popup").addEventListener("click",function(e){
+  if(e.target.id === "popup"){
+    closePopup();
+  }
+});
+});
